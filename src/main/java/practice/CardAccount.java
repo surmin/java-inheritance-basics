12ef02939e9529ee1885b89ccb7d5aa4abcf1b77
Yuri Surmin <yura.surmin@gmail.com>
2025-02-17 23:42:35 +0700

    codestyle mods

--- src/main/java/practice/CardAccount.java
+++ src/main/java/practice/CardAccount.java
@@ -4,7 +4,7 @@ public class CardAccount extends BankAccount {
 
     @Override
     public void take(double amountToTake) {
-        if (amount - amountToTake * 1.01 >=0) {
+        if (amount - amountToTake * 1.01 >= 0) {
             amount -= amountToTake * 1.01;
         }
     }
